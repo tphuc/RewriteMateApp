c63bbb840522b4f9ec328e7c70cf0e707befd6b1
FelixTran <felixtran2000@gmail.com>
2026-05-21 16:04:28 +0700

    update

---
+++ app/(marketing)/backlinks.tsx
@@ -0,0 +1,21 @@
+export default function Backlinks() {
+  return (
+          <div className="flex items-center gap-2 justify-center py-4 border-t border-border/50 bg-secondary/50 border-b w-full overflow-x-scroll scrollbar-hide no-scrollbar">
+        <a href="https://twelve.tools" target="_blank">
+          <img src="https://twelve.tools/badge0-white.svg" alt="Featured on Twelve Tools" width="200" height="54" />
+        </a>
+        <a href="https://sidehunt.io/project/rewritemate-ai-keyboard" target="_blank" rel="noopener">
+          <img src="https://sidehunt.io/badges/sidehunt-badge-light.svg" alt="Featured on Sidehunt" width="200" height="60" />
+        </a>
+        <a href="https://startuups.com//projects/rewritemate-ai-keyboard" target="_blank" rel="noopener">
+          <img src="https://startuups.com//images/badges/startuupscom.badge.svg" alt="Featured on startuups" width="150" height="54" />
+        </a>
+        <a href="https://sellwithboost.com" target="_blank" rel="noopener noreferrer">
+          <img src="https://sellwithboost.com/badge/listing.svg" alt="Listed on Sell With boost" style={{ height: 40 }} />
+        </a>
+        <a href="https://postyourstartup.co/startup/rewritemate?ref=badge" target="_blank">
+          <img src="https://postyourstartup.co/api/badge/rewritemate?theme=light" alt="Featured on PostYourStartup" width="212" height="55" />
+        </a>
+      </div>
+  );
+}
--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -6,6 +6,7 @@ import Features from "./features";
 import IOSFeatures from "./ios-features";
 import PricingCard from "./pricing";
 import Footer2 from "./footer";
+import Backlinks from "./backlinks";
 
 const roles = [
   {
@@ -182,20 +183,7 @@ export default function Home() {
       <FaqSection />
       {/* <Footer /> */}
 
-      <div className="flex items-center gap-2 justify-center py-4 border-t border-border/50 bg-secondary/50 border-b w-full overflow-x-scroll scrollbar-hide no-scrollbar">
-        <a href="https://twelve.tools" target="_blank">
-          <img src="https://twelve.tools/badge0-white.svg" alt="Featured on Twelve Tools" width="200" height="54" />
-        </a>
-        <a href="https://sidehunt.io/project/rewritemate-ai-keyboard" target="_blank" rel="noopener">
-          <img src="https://sidehunt.io/badges/sidehunt-badge-light.svg" alt="Featured on Sidehunt" width="200" height="60" />
-        </a>
-        <a href="https://startuups.com//projects/rewritemate-ai-keyboard" target="_blank" rel="noopener">
-          <img src="https://startuups.com//images/badges/startuupscom.badge.svg" alt="Featured on startuups" width="150" height="54" />
-        </a>
-        <a href="https://sellwithboost.com" target="_blank" rel="noopener noreferrer">
-  <img src="https://sellwithboost.com/badge/listing.svg" alt="Listed on Sell With boost" style={{height:40}} />
-</a>
-      </div>
+      <Backlinks />
       <Footer2 />
     </div>
   );
