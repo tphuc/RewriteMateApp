851ea4a5d56b5a2805ab4a52b1e2032b491b402c
Tran Bao Phuc <FelixTran2000@gmail.com>
2026-08-31 02:08:56 +0700

    update

--- app/(marketing)/backlinks.tsx
+++ app/(marketing)/backlinks.tsx
@@ -1,25 +1,30 @@
 export default function Backlinks() {
   return (
-          <div className="flex items-center gap-2 justify-center py-4 border-t border-border/50 bg-secondary/50 border-b w-full overflow-x-scroll scrollbar-hide no-scrollbar">
-        <a href="https://twelve.tools" target="_blank">
-          <img src="https://twelve.tools/badge0-white.svg" alt="Featured on Twelve Tools" width="200" height="54" />
-        </a>
-        <a href="https://sidehunt.io/project/rewritemate-ai-keyboard" target="_blank" rel="noopener">
-          <img src="https://sidehunt.io/badges/sidehunt-badge-light.svg" alt="Featured on Sidehunt" width="200" height="60" />
-        </a>
-        <a href="https://startuups.com//projects/rewritemate-ai-keyboard" target="_blank" rel="noopener">
-          <img src="https://startuups.com//images/badges/startuupscom.badge.svg" alt="Featured on startuups" width="150" height="54" />
-        </a>
-        <a href="https://sellwithboost.com" target="_blank" rel="noopener noreferrer">
-          <img src="https://sellwithboost.com/badge/listing.svg" alt="Listed on Sell With boost" style={{ height: 40 }} />
-        </a>
-        <a href="https://postyourstartup.co/startup/rewritemate?ref=badge" target="_blank">
-          <img src="https://postyourstartup.co/api/badge/rewritemate?theme=light" alt="Featured on PostYourStartup" width="212" height="55" />
-        </a>
+    <div className="flex items-center gap-2 justify-center py-4 border-t border-border/50 bg-secondary/50 border-b w-full overflow-x-scroll scrollbar-hide no-scrollbar">
+      <a href="https://twelve.tools" target="_blank">
+        <img src="https://twelve.tools/badge0-white.svg" alt="Featured on Twelve Tools" width="200" height="54" />
+      </a>
+      <a href="https://sidehunt.io/project/rewritemate-ai-keyboard" target="_blank" rel="noopener">
+        <img src="https://sidehunt.io/badges/sidehunt-badge-light.svg" alt="Featured on Sidehunt" width="200" height="60" />
+      </a>
+      <a href="https://startuups.com//projects/rewritemate-ai-keyboard" target="_blank" rel="noopener">
+        <img src="https://startuups.com//images/badges/startuupscom.badge.svg" alt="Featured on startuups" width="150" height="54" />
+      </a>
+      <a href="https://sellwithboost.com" target="_blank" rel="noopener noreferrer">
+        <img src="https://sellwithboost.com/badge/listing.svg" alt="Listed on Sell With boost" style={{ height: 40 }} />
+      </a>
+      <a href="https://postyourstartup.co/startup/rewritemate?ref=badge" target="_blank">
+        <img src="https://postyourstartup.co/api/badge/rewritemate?theme=light" alt="Featured on PostYourStartup" width="212" height="55" />
+      </a>
 
-        <a href="https://buildfinds.com/projects/rewritemate?utm_source=badge" target="_blank" rel="noopener noreferrer">
-  <img src="https://buildfinds.com/buildfinds/images/badges/featured-on-light.svg" alt="Featured on Build Finds" />
-</a>
-      </div>
+      <a href="https://buildfinds.com/projects/rewritemate?utm_source=badge" target="_blank" rel="noopener noreferrer">
+        <img src="https://buildfinds.com/buildfinds/images/badges/featured-on-light.svg" alt="Featured on Build Finds" />
+      </a>
+
+      <a href="https://aitoolboxer.com/projects/protato?utm_source=badge" target="_blank" rel="noopener noreferrer">
+        <img src="https://aitoolboxer.com/aitoolboxer/images/badges/featured-on-light.svg" alt="Featured on AI Tool Boxer"  />
+      </a>
+
+    </div>
   );
 }
